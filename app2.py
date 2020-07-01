580c96e40528b2bdc98ae49c6503199a04e956f2
komalivelaga <komali.velaga@gmail.com>
2020-07-01 13:57:05 +0530

    Added month and days calculation.

--- app2.py
+++ app2.py
@@ -6,3 +6,6 @@ year = int(input("please enter the year you are born"))
 print(year)
 cureentage = 2020 - year
 print("you are %s old", currentage)
+months = currentage * 12
+days = currentage * 365
+print("Hello"+ name +". you are %d (months)) days old" % (days))
