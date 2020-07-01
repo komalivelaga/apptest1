05b3e46a2dc0a5c6dd24638eaf61bde7be6d7571
komalivelaga <komali.velaga@gmail.com>
2020-07-01 13:01:08 +0530

    Added code for age calculation

--- app2.py
+++ app2.py
@@ -1,2 +1,8 @@
 #calculate persons age based on year of birth
 year = int(input("please enter the year you are born"))
+print(year)
+name = str(input("please enter your good name"))
+year = int(input("please enter the year you are born"))
+print(year)
+cureentage = 2020 - year
+print("you are %s old", currentage)
